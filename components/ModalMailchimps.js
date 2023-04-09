bf82e11dd647a952d697a3ca21dd50b3f9fa77c8
jgwentworth92 <alphabetboi92@protonmail.com>
2023-04-08 21:26:21 -0400

    layout changes

--- components/ModalMailchimps.js
+++ components/ModalMailchimps.js
@@ -1,56 +1,87 @@
 import React from 'react';
-
+import { Dialog, Transition } from '@headlessui/react';
 export default function ModalMailchimps({ isOpen, onClose, onSubmit }) {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40">
-      <div className="bg-white p-6 rounded-lg w-full max-w-md">
-        <h2 className="text-xl font-bold mb-4">Subscribe</h2>
-        <form onSubmit={onSubmit}>
-          <div className="mb-4">
-            <label htmlFor="firstName" className="block text-sm mb-2">First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              className="w-full p-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="lastName" className="block text-sm mb-2">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              className="w-full p-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm mb-2">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="w-full p-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded-md"
+    <Transition show={isOpen} as={React.Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-50 overflow-y-auto"
+        onClose={onClose}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-40" />
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
           >
-            Subscribe
-          </button>
-        </form>
-        <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          onClick={onClose}
-        >
-          &times;
-        </button>
-      </div>
-    </div>
+            &#8203;
+          </span>
+          <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-lg">
+            <Dialog.Title
+              as="h3"
+              className="text-lg font-medium leading-6 text-gray-900"
+            >
+              Subscribe
+            </Dialog.Title>
+            <form onSubmit={onSubmit} className="mt-4">
+              {/* First Name */}
+              <div className="mb-4">
+                <label htmlFor="firstName" className="block text-sm mb-2">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              {/* Last Name */}
+              <div className="mb-4">
+                <label htmlFor="lastName" className="block text-sm mb-2">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              {/* Email */}
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-sm mb-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-md"
+              >
+                Subscribe
+              </button>
+            </form>
+            <button
+              type="button"
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+              onClick={onClose}
+            >
+              &times;
+            </button>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
--- components/hero.js
+++ components/hero.js
@@ -1,48 +1,80 @@
 
-import Image from 'next/image';
+
+
+
+
+
+
+    import Image from 'next/image';
 import Link from 'next/link';
-import CustomButton from './CustomButton';
+
 import React, { useState } from 'react';
 import ModalMailchimps from './ModalMailchimps';
+import * as Toast from '@radix-ui/react-toast';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 export default function HeroComp({ title, subtitle, backgroundImage, ctaText, ctaLink }) {
-    const [modalOpen, setModalOpen] = useState(false);
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-      
-        const data = {
-          email: e.target.email.value,
-          firstName: e.target.firstName.value,
-          lastName: e.target.lastName.value,
-        };
-      
-        try {
-          const response = await fetch('/api/subscribe', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-          });
-      
-          if (response.ok) {
-            setModalOpen(false);
-          } else {
-            // Handle error (e.g., show error message)
-          }
-        } catch (error) {
-          // Handle fetch error (e.g., show error message)
-        }
-      };
-      return (
-        <section className="relative">
-          <div className="relative w-full h-[500px]">
+  const [modalOpen, setModalOpen] = useState(false);
+  const [toastOpen, setToastOpen] = useState(false);
+  const [toastContent, setToastContent] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: e.target.email.value,
+      firstName: e.target.firstName.value,
+      lastName: e.target.lastName.value,
+    };
+
+    try {
+      const response = await fetch('/api/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setModalOpen(false);
+        setToastContent('Subscription successful!');
+        setToastOpen(true);
+      } else {
+        setToastContent('Subscription failed. Please try again.');
+        setToastOpen(true);
+      }
+    } catch (error) {
+      setToastContent('An error occurred. Please try again.');
+      setToastOpen(true);
+    }
+  };
+
+  return (
+    
+    <section className="relative heroComp">
+         <Toast.Provider>
+        <Toast.Root className="ToastRoot" open={toastOpen} onOpenChange={setToastOpen}>
+          <Toast.Title className="ToastTitle">Notification</Toast.Title>
+          <Toast.Description asChild>
+            <p className="ToastDescription">{toastContent}</p>
+          </Toast.Description>
+          <Toast.Action className="ToastAction" asChild altText="Close">
+            <button className="Button small green" onClick={() => setToastOpen(false)}>
+              Close
+            </button>
+          </Toast.Action>
+        </Toast.Root>
+        <Toast.Viewport className="ToastViewport" />
+      </Toast.Provider>
+        <div className="relative w-full h-[500px]">
             <Image
               src={backgroundImage}
-              layout="fill"
+              fill
               objectFit="cover"
               objectPosition="center"
               alt={title}
             />
+            
           </div>
           <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50" />
           <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-start pt-6 pl-6">
@@ -60,7 +92,13 @@ export default function HeroComp({ title, subtitle, backgroundImage, ctaText, ct
               </button>
             )}
           </div>
-          < ModalMailchimps isOpen={modalOpen} onClose={() => setModalOpen(false)} onSubmit={handleSubmit} />
-        </section>
-      );
-    }
+
+      <ModalMailchimps
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onSubmit={handleSubmit}
+      />
+
+    </section>
+  );
+}
