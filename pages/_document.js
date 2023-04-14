e8bd260ad65b75a59c19829a87d530510726699b
jgwentworth92 <alphabetboi92@protonmail.com>
2023-04-14 15:22:00 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,20 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+     <script async src={`https://www.googletagmanager.com/gtag/js?id=${"G-0NXCE038ST"}`} />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${"G-0NXCE038ST"}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </body>
     </Html>
   )
