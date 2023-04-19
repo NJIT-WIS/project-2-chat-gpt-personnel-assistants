c5f1dc7cd9bb6892d666977fd416ff586c2e4102
jgwentworth92 <alphabetboi92@protonmail.com>
2023-04-19 15:24:16 -0400

    removing unused components

--- components/RichText.js
+++
@@ -1,9 +0,0 @@
-import BlockContent from "@sanity/block-content-to-react";
-
-export default function RichText({ data }) {
-  return (
-    <div className="prose mb-8">
-      <BlockContent blocks={data.content} />
-    </div>
-  );
-}
