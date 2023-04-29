57a7d801027d04404edfdf0c95e38b0ccaca7c07
Frank Xu <flx2@njit.edu>
2023-04-28 21:37:24 -0400

    modified multi_language test for more flexibility

--- tests/playwright_tests/multi_language.spec.js
+++ tests/playwright_tests/multi_language.spec.js
@@ -9,7 +9,7 @@ const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'));
 const TIMEOUT = 30000;
 pages.forEach((page) => {
 
-  test(`Page "${page.path}" should have only 1 language`, async ({}) => {
+  test(`Page "${page.path}" should support languages`, async ({}) => {
     console.log(page.path)
     const pageUrl = `${config.use.baseURL}${page.path}`;
 
@@ -22,28 +22,29 @@ async function checkPageMultiLang(pageUrl) {
   const page = await browser.newPage();
   await page.goto(pageUrl, { timeout: TIMEOUT });
 
-  const languages = ['en', 'es']; // Array of language codes to check for
+  const languages = ['en']; // Array of language codes to check for
 
-  const hasLanguage = await page.evaluate(() => {
-  // Search for elements that contain English text
-  const englishSelectors = [
-    'html[lang="en"]', // Look for the "lang" attribute on the <html> tag
-    '[lang="en"]', // Look for elements with the "lang" attribute set to "en"
-    '[class*="lang-en"]', // Look for elements with a class that contains "lang-en"
-    'body:contains("English")', // Look for the string "English" within the <body> tag
-    // Add other selectors as needed to match language indicators on the page
-  ];
+  const hasLanguage = await page.evaluate((languages) => {
+    const languageSelectors = languages.map(lang => (
+      `html[lang="${lang}"],
+      [lang="${lang}"],
+      [class*="lang-${lang}"],
+      body`
+    )).join(',');
 
-  // Check if any of the English selectors match an element on the page
-  for (const selector of englishSelectors) {
-    const element = document.querySelector(selector);
-    if (element) {
-      return true;
+    const elements = document.querySelectorAll(languageSelectors);
+
+    for (const element of elements) {
+      if (element.textContent.toLowerCase().includes(languages.join('|').toLowerCase())) {
+        return true;
+      }
     }
-  }
+    return false;
+  }, languages);
 
-  // If no English language indicators are found, return false
-  return false;
-  });
   await browser.close();
+
+  if (!hasLanguage) {
+    throw new Error(`None of the specified languages (${languages.join(', ')}) were found on the page.`);
+  }
 }
